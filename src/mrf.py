6b4d8aac1706350aac8f429e4b7548be92950f71
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-12-19 15:38:06 +0400

    MRF Model testing

---
+++ src/mrf.py
@@ -0,0 +1,138 @@
+from visualize import *
+import scipy.stats as sc
+
+#Implement the RGA BS algorithm
+#Initialise the constants
+n = 0.01
+mean = np.zeros((235,24,32,16))
+mean[0,:,:,0] = np.copy(data[0,:,:,0])
+sigma = 0.4
+alpha = 0.01
+M = 0
+gmean = np.zeros((235,24,32,16))
+gp = np.zeros((235,24,32,16))
+event = np.zeros((235,24,32,16))
+
+for i in range(0,1):
+    for j in range(0,1):
+        for k in range(0,24):
+            for l in range(0,32):
+                event[i,k,l,j] = 1
+
+for j in range(0,16):
+    mean[0,:,:,0] = mean[0,:,:,0] + data[0,:,:,j]
+for j in range(0,9):
+    mean[0,:,:,0] = mean[0,:,:,0] + data[1,:,:,j]
+
+mean[0,:,:,0] = mean[0,:,:,0]/25
+
+fg = []
+cnt = np.zeros((236,16))
+bg = []
+maxi = np.zeros((235,24,32,16))
+mini = np.zeros((235,24,32,16))
+mini[0,:,:,0] = data[0,:,:,0]
+maxi[0,:,:,0] = data[0,:,:,0]
+theta = 0.015
+gamma = 0.2
+
+#For the frames, calculate the running average
+
+for l in range(0,235):
+    start = 0
+    if l == 0:
+        start = 1
+    for k in range(start,16):
+        for i in range(0,24):
+            for j in range(0,32):
+                if k==0:
+                    mean[l,i,j,k] = alpha*data[l,i,j,k] + mean[l-1,i,j,15]*(1-alpha)    ## Mean if k==0 and l!=0
+                    maxi[l,i,j,k] = max(maxi[l-1,i,j,15] , data[l,i,j,k])
+                    mini[l,i,j,k] = min(mini[l-1,i,j,15] , data[l,i,j,k])
+                else:
+                    mean[l,i,j,k] = alpha*data[l,i,j,k] + mean[l,i,j,k-1]*(1-alpha)     ## Mean if k!=0 and l!=0
+                    maxi[l,i,j,k] = max(maxi[l,i,j,k-1] , data[l,i,j,k])
+                    mini[l,i,j,k] = min(mini[l,i,j,k-1] , data[l,i,j,k])
+
+                gp[l,i,j,k] = gaussian_pdf(data[l,i,j,k] , sigma , mean[l,i,j,k])       ## Find the gaussian PDF 
+                if gp[l,i,j,k]<n and data[l,i,j,k]>mean[l,i,j,k]:                       ## Process is foreground
+                    event[l,i,j,k] = 0
+                    fg.append((data[l,i,j,k] , mean[l,i,j,k]))
+                    if k!=0:
+                        mean[l,i,j,k] = mean[l,i,j,k-1]                                 ## Update the mean if the process is foreground
+                    else:
+                        mean[l,i,j,k] = mean[l-1,i,j,15]
+
+                else:
+                    event[l,i,j,k] = 1   
+                    bg.append((data[l,i,j,k] , mean[l,i,j,k]))                          ## Process is Background
+
+
+diff = np.zeros((235,24,32,16))
+mrf_event = np.zeros((235,24,32,16))
+mrf_event[0,:,:,0] = 1
+for l in range(0,235):
+    for k in range(0,16):
+        for i in range(0,24):
+            for j in range(0,32):
+                qf = 0
+                qb = 0
+                ratio = gp[l,i,j,k]*25
+                if i>0 and j>0:
+                    if event[l,i-1,j-1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if i>0:
+                    if event[l,i-1,j,k] == 0:
+                        qf = qf+1
+                    else:
+                        qb = qb+1
+                if j<31 and i>0:
+                    if event[l,i-1,j+1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if j>0:
+                    if event[l,i,j-1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if j<31:
+                    if event[l,i,j+1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if i<23 and j>0:
+                    if event[l,i+1,j-1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if i<23:
+                    if event[l,i+1,j,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+                if i<23 and j<31:
+                    if event[l,i+1,j+1,k] == 0:
+                        qf = qf + 1
+                    else:
+                        qb = qb + 1
+
+                mrf = theta * np.exp((qf-qb)/gamma)
+                if ratio < mrf:
+                    mrf_event[l,i,j,k] = 0
+                    cnt[l,k] = cnt[l,k] + 1
+                else:
+                    mrf_event[l,i,j,k] = 1
+                diff[l,i,j,k] = ratio-mrf
+
+for l in range(0,235):
+    for k in range(0,16):
+        if cnt[l,k] > 500:
+            print(l,k)
+
+
+        
+
+
